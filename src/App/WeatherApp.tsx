3582a14659f1751249b50355ad2c37b9fa2ea3d2
Aziz Zainutdin <aloha@scriptor.me>
2017-12-29 14:17:00 +0300

    Added sccs. Trying to find out how webpack is works

--- src/App/WeatherApp.tsx
+++ src/App/WeatherApp.tsx
@@ -3,7 +3,8 @@ import * as React from 'react';
 export class WeatherApp extends React.Component<{}, {}> {
     render() {
         return (
-            <div>
+            <div className="application-container">
+                <div className="application-container__background" />
                 <h1>Aloha</h1>
             </div>
         );
