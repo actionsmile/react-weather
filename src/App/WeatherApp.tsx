a67e5d991407a4aa4b0525ca9703caa711dd6a31
Aziz Zainutdin <aloha@scriptor.me>
2018-01-05 05:55:49 +0300

    Added reducer test and renewed webpack config

--- src/App/WeatherApp.tsx
+++ src/App/WeatherApp.tsx
@@ -9,10 +9,7 @@ interface IWeatherState {
 export class WeatherApp extends React.Component<IWeatherProps, IWeatherState> {
     render() {
         return (
-            <div className="application-container">
-                <div className="application-container__background" />
-                <h1>Aloha</h1>
-            </div>
+            <div />
         );
     }
 }
--- src/__tests__/WeatherApp.test.tsx
+++ src/__tests__/WeatherApp.test.tsx
@@ -1,8 +1,36 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import * as Reducers from './../App/Reducers/BasicReducer';
 import { WeatherApp } from './../App/WeatherApp';
+import {
+    EBasicActionType,
+    IAppState,
+    IBasicAction
+} from './../Core/Models';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<WeatherApp />, div);
+    const div = document.createElement('div');
+    ReactDOM.render(<WeatherApp />, div);
+});
+
+it('Incrementing passes', () => {
+    const dispatchingAction: IBasicAction = {
+        type: EBasicActionType.INCREMENT,
+        value: 1
+    }
+
+    expect(Reducers.counter(null, dispatchingAction)).toEqual({ ...Reducers.init.state, counter: 1 });
+});
+
+it('Decrement passes', () => {
+    const state: IAppState = {
+        city: 'Moscow',
+        counter: 2
+    };
+    const dispatchingAction: IBasicAction = {
+        type: EBasicActionType.DECREMENT,
+        value: 1
+    }
+
+    expect(Reducers.counter(state, dispatchingAction)).toEqual({ ...state, counter: 1 });
 });
