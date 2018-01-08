a03b2c8d111d344934459e2923ccbc1911cef3f4
Aziz Zainutdin <aloha@scriptor.me>
2018-01-08 23:21:42 +0300

    Fixed test passing

--- __tests__/WeatherApp.test.tsx
+++ __tests__/WeatherApp.test.tsx
@@ -1,43 +1,8 @@
-import * as deepFreeze from 'deep-freeze';
 import * as React from 'react';
 import { render } from 'react-dom';
-import * as Reducers from './../src/App/Reducers/BasicReducer';
 import { WeatherApp } from './../src/App/WeatherApp';
-import {
-    EBasicActionType,
-    IBasicAction,
-    ICounterState
-} from './../src/Core/Models';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
     render(<WeatherApp />, div);
-});
-
-it('Initial state works even if `null` used as argument passes', () => {
-    const finalState: ICounterState = {
-        value: 1
-    }
-    const dispatchingAction: IBasicAction = {
-        type: EBasicActionType.INCREMENT,
-        value: 1
-    }
-    deepFreeze(dispatchingAction);
-    expect(Reducers.counter(null, dispatchingAction)).toEqual(finalState);
-});
-
-it('Decrement passes', () => {
-    const initialState: ICounterState = {
-        value: 2
-    };
-    const finalState: ICounterState = {
-        value: 1
-    }
-    const dispatchingAction: IBasicAction = {
-        type: EBasicActionType.DECREMENT,
-        value: 1
-    }
-    deepFreeze(initialState);
-    deepFreeze(dispatchingAction);
-    expect(Reducers.counter(initialState, dispatchingAction)).toEqual(finalState);
 });
